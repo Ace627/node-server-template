01bc3a4328d7a207f518e0dc1e28bdfe6fdcedcb
legendary <1207588603@qq.com>
2023-05-10 18:24:46 +0800

    配置全局路由前缀

--- src/config/deault.config.ts
+++ src/config/deault.config.ts
@@ -1,1 +1,3 @@
-export const Server_Port = 9999
+export const SEVER_PORT = 9999 // 服务监听端口
+
+export const BASE_URL = '/node-api' // 全局路由前缀
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import ExceptionInterceptor from './config/exception.interceptor'
 import ResponseInterceptor from './config/response.interceptor'
-import { Server_Port } from './config/deault.config'
+import { SEVER_PORT, BASE_URL } from './config/deault.config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.setGlobalPrefix(BASE_URL)
   // 配置静态资源目录
   app.useStaticAssets('public')
   // 配置全局异常拦截器
   app.useGlobalFilters(new ExceptionInterceptor())
   // 配置全局响应拦截器
   app.useGlobalInterceptors(new ResponseInterceptor())
-  await app.listen(Server_Port)
-  console.log(`Node Server is running at http://localhost:${Server_Port}`)
+  await app.listen(SEVER_PORT)
+  console.log(`Node Server is running at http://localhost:${SEVER_PORT}${BASE_URL}`)
 }
 bootstrap()
