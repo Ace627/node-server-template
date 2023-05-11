e8e8728af49a6b27fbd01925cd3f9452e4da2aae
legendary <1207588603@qq.com>
2023-05-11 11:27:10 +0800

    新增接口：随机一条诗词名句

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service'
 import dayjs from 'dayjs'
 import crypto from 'crypto'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import poemList from '@/db/poems.json'
 
 @ApiTags('工具类接口')
 @Controller()
@@ -13,5 +14,11 @@ export class AppController {
   @ApiOperation({ summary: '随机生成一个 uuid' })
   getUUID(): string {
     return crypto.randomUUID()
+  }
+
+  @Get('/tools/poem')
+  @ApiOperation({ summary: '随机一条诗词名句' })
+  getRandomPoem(): any {
+    return poemList[Math.floor(Math.random() * poemList.length)]
   }
 }
