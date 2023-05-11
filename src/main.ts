d8b1cdd4284994336fb7183a284fbeab36a878bf
legendary <1207588603@qq.com>
2023-05-11 15:41:18 +0800

    修复 swagger 接口文档路径异常的问题

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import SwaggerConfig from './config/swagger.config'
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.setGlobalPrefix(BASE_URL) // 添加全局路由前缀
-  SwaggerConfig(app, '/swagger-api')
+  SwaggerConfig(app, API_DOC_URL)
   app.useStaticAssets('public') // 配置静态资源目录
   app.useGlobalFilters(new ExceptionInterceptor()) // 配置全局异常拦截器
   app.useGlobalInterceptors(new ResponseInterceptor()) // 配置全局响应拦截器
