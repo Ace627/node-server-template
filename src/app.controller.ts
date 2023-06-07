32f7d7102ded85bf7e3786f408a67f9e249b9599
legendary <1207588603@qq.com>
2023-06-07 16:28:08 +0800

    新增随机颜色的接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common'
-import { AppService } from './app.service'
-import dayjs from 'dayjs'
 import crypto from 'crypto'
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import poemList from '@/db/poems.json'
+import { Controller, Get, Query } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
+import { AppService } from './app.service'
 
 @ApiTags('工具类接口')
 @Controller()
@@ -16,10 +14,16 @@ export class AppController {
     return crypto.randomUUID()
   }
 
-  @Get('/tools/poem')
-  @ApiOperation({ summary: '随机一条诗词名句' })
-  getRandomPoem(): any {
-    const record = poemList[Math.floor(Math.random() * poemList.length)]
-    return record
+  @Get('/tools/randomHexColor')
+  @ApiOperation({ summary: '随机生成一个 Hex 格式的颜色' })
+  getRandomHexColor(): string {
+    return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`
+  }
+
+  @Get('/tools/randomRgbColor')
+  @ApiOperation({ summary: '随机生成一个 Rgb 格式的颜色' })
+  getRandomRgbColor(): string {
+    const random = (): number => Math.floor(Math.random() * 256)
+    return `rgb(${random()}, ${random()}, ${random()})`
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import ResponseInterceptor from './common/response.interceptor' // 全局响应拦截器
 import ExceptionInterceptor from './common/exception.interceptor' // 全局异常拦截器
-import SwaggerConfig from './config/swagger.config'
+import SwaggerConfig from './common/swagger.config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -14,14 +15,21 @@ async function bootstrap() {
   // 配置全局路由前缀
   // http://localhost:3001/demo => http://localhost:3001/api/demo
   // app.setGlobalPrefix('/api')
+
+  // 配置 swagger 接口文档
   SwaggerConfig(app, 'api-docs')
-  app.useStaticAssets('public') // 配置静态资源目录
+
+  // 配置静态资源目录
+  app.useStaticAssets('public')
 
   // 配置全局响应拦截器
   app.useGlobalInterceptors(new ResponseInterceptor())
 
   // 配置全局异常拦截器
   app.useGlobalFilters(new ExceptionInterceptor())
+
+  // 配置全局验证管道
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(3000)
 
