3f8324095e13045d9cce2b9eecd3074e924b579c
legendary <1207588603@qq.com>
2023-06-07 18:35:12 +0800

    接口文档换为 knife4j UI 界面

--- src/common/swagger.config.ts
+++ src/common/swagger.config.ts
@@ -1,8 +1,16 @@
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { knife4jSetup } from 'nestjs-knife4j'
 import type { NestExpressApplication } from '@nestjs/platform-express'
 
 export default (app: NestExpressApplication, path: string) => {
-  const options = new DocumentBuilder().setTitle('API 文档的标题').setDescription('API 文档的描述').setVersion('0.0.1').build()
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs 服务器模板')
+    .setContact('当时只道是寻常', '', '')
+    .setDescription('API 文档的描述')
+    .setVersion('0.0.1')
+    .build()
+
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup(path, app, document)
+  knife4jSetup(app, { urls: [{ name: 'admin api', url: `/${path}-json`, swaggerVersion: '3.0', location: `/${path}-json` }] })
 }
