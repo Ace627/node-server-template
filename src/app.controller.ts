2ee3955e2cf06f089f9e102bba35d796b5f259f7
legendary <1207588603@qq.com>
2023-06-07 17:42:20 +0800

    修改 nest 命令配置为默认不生成 spec 测试文件

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Query } from '@nestjs/common'
-
 import { AppService } from './app.service'
 
+/**
+ * 控制层，主要是写路由相关代码以及处理前端传来的一些参数
+ */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common'
 
+/**
+ * 业务层，写一些与业务相关的逻辑。比如对数据库的 CRUD 就可以写到这里
+ */
 @Injectable()
 export class AppService {
   getHello(): string {
